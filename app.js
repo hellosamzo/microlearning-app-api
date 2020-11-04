55c86f73817cb469e75389354729f9b0b434b418
Sam <samuelmatthews96@gmail.com>
2020-11-04 20:54:38 +0000

    Added proper error handling with Morgan library

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const genreRoute = require('./api/routes/genres');
 const urlRoute = require('./api/routes/urls');
 
+// log requests in console
+app.use(morgan('dev'));
+
 // routes
 app.use('/genres', genreRoute);
 app.use('/urls', urlRoute);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found..');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
